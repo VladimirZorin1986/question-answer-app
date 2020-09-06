bd7e7b9258e004087d4526ee50ed60b2ee652bbe
VladimirZorin1986 <pls.github@gmail.com>
2020-09-06 22:48:02 +0300

    version 1.0.8

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = b'\xea\xf8\x10\xa0\x1a\xc82\xc5quN\xde2\x1d&\x920\\`\xa6\x97\x0c\xf7\xd2'
